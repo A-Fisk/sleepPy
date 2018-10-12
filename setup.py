e1f8221a2977b414655a57cdb16eb88cb262280a
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2018-10-12 14:37:56 +0100

    Adds basic required files

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sleepPy",
+    version="0.0.1",
+    author="Angus Fisk", 
+    author_email="angus_fisk@hotmail.com",
+    description="scripts for analysis of sleep EEG data", 
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/A-Fisk/sleepPy", 
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3.0",
+        "Operating System :: OS Independent",
+    ],
+)
