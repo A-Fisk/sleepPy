00d588386311433fd14c2d9a5ca38bf4db4831c9
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2018-10-12 16:19:19 +0100

    Updates master before working on issue 2

---
+++ sleepPy/preprocessing.py
@@ -0,0 +1,6 @@
+# Script for preprocessing EEG data
+# that has been scored and outputed
+# as time series with labels and fourier
+# transform power in columns
+# single file for each animal/day to analyse
+
