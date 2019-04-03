6b202ed35c6da20cc821e1e8e14def9caf879006
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2019-04-03 11:28:30 +0100

    Significance functions
    Adds in get_xtick_dict
    get ycoords
    get label_loc_dict
    draw_sighlines
    functions - all needed to easily draw significance lines

--- sleepPy/plots.py
+++ sleepPy/plots.py
@@ -1,6 +1,7 @@
 # Script for plotting data from EEG frequency bands
 
 import pandas as pd
+idx = pd.IndexSlice
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -441,3 +442,83 @@ def dark_light_plot(data: pd.DataFrame,
     
     return fig, ax, params_dict
 
+
+def draw_sighlines(yval: float,
+                   sig_list: list,
+                   label_loc_dict: dict,
+                   minus_val: float,
+                   plus_val: float,
+                   curr_ax,
+                   **kwargs):
+    """
+    Draws an hline at the position indicated in the sig list of indexes by
+    looking it up in label_loc_dict then drawing a hline at the yval from
+    loc mins_val to loc plus_val on curr_ax
+    :param yval:
+    :param sig_list:
+    :param label_loc_dict:
+    :param minus_val:
+    :param plus_val:
+    :param curr_ax:
+    :return:
+    """
+    for xval in sig_list:
+        curr_xval = label_loc_dict[xval]
+        hxvals = [curr_xval - minus_val, curr_xval + plus_val]
+        hxval_axes = curr_ax.transLimits.transform([(hxvals[0], 0),
+                                                    (hxvals[1], 0)])
+        hxval_axes_val = hxval_axes[:, 0]
+        curr_ax.axhline(yval,
+                        xmin=hxval_axes_val[0],
+                        xmax=hxval_axes_val[1],
+                        **kwargs)
+
+def get_xtick_dict(curr_ax):
+    """
+    Returns a dict of the locations of where each label is
+    Can then be used to find where to draw liens
+    :param curr_ax:
+    :return:
+    """
+    labels = curr_ax.get_xticklabels()
+    locs = curr_ax.get_xticks()
+    label_text = [x.get_text() for x in labels]
+    label_loc_dict = dict(zip(label_text, locs))
+    return label_loc_dict
+
+
+def sig_locs_get(df: pd.DataFrame,
+                 sig_col: str = "p-tukey",
+                 sig_val: float = 0.05,
+                 index_level2val: int = 0):
+    """
+    Finds where the sig_col is less than the sig_val for the index_level2val
+    Used for a posthoc tukeys df as example where 0 =baseline-disrupted
+    1=baseline-recovery etc.
+    :param df:
+    :param sig_col:
+    :param sig_val:
+    :param index_level2val:
+    :return:
+    """
+    sig_mask = df[sig_col] < sig_val
+    sig_vals = df[sig_mask]
+    sig_index = sig_vals.loc[idx[:, index_level2val], :
+                ].index.get_level_values(0)
+    return sig_index
+
+
+def sig_line_coord_get(curr_ax,
+                       sig_line_ylevel: float = 0.9):
+    """
+    Finds the sig_line_level in axes co-ordinates
+    :param curr_ax:
+    :param sig_line_ylevel:
+    :return:
+    """
+    axes_to_data = curr_ax.transLimits.inverted()
+    ycoords = (0.5, sig_line_ylevel)
+    ycoords_data = axes_to_data.transform(ycoords)
+    ycoord_data_val = ycoords_data[1]
+    
+    return ycoord_data_val
